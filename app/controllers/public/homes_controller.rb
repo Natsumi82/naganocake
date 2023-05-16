7f979a891e3e39e3c44339983f91c544904e1b6b
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-05-16 14:42:09 +0000

    20230513

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
-
+    @items = Item.joins(:genres).where(genres: { is_active: true }).where(is_active: true).order("RANDOM()").limit(6)
     @genres = Genre.where(is_active: true)
   end
 
   def about
-
+     @genres = Genre.where(is_active: true)
   end
 end
